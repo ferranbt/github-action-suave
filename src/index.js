63e0ec15b230dbc9dbe7278396c141e926d996c2
Ferran Borreguero <ferranbt@protonmail.com>
2024-01-08 11:15:40 +0100

    DD

--- src/index.js
+++ src/index.js
@@ -2,24 +2,22 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 const { exec } = require("child_process");
 
-try {
-  exec(
-    "curl https://gist.githubusercontent.com/ferranbt/9b2765236b1f4297dd06e9e02d3c3432/raw/76e171abe25c79adf8cf4268f792754c4959e7d1/suaveup.sh | bash",
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.error(`stderr: ${stderr}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-    },
-  );
-} catch (error) {
-  core.setFailed(error.message);
-}
+console.log("Now?");
+
+exec(
+  "curl https://gist.githubusercontent.com/ferranbt/9b2765236b1f4297dd06e9e02d3c3432/raw/76e171abe25c79adf8cf4268f792754c4959e7d1/suaveup.sh | bash",
+  (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  },
+);
 
 try {
   // `who-to-greet` input defined in action metadata file
